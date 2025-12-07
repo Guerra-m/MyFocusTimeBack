e14cdfcf5c32d6839bbf397cac0d298dcf80e0c0
Martin Guerra Local <vernonguerra27@gmail.com>
2025-12-07 12:39:45 -0300

    mejoras

--- src/main/java/com/guerram/MyFocusTime/MyFocusTimeApplication.java
+++ src/main/java/com/guerram/MyFocusTime/MyFocusTimeApplication.java
@@ -2,7 +2,6 @@ package com.guerram.MyFocusTime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class MyFocusTimeApplication {
 
