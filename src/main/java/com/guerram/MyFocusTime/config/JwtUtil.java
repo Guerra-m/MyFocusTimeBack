1f7591408e12198bbced66a29aad86c02a0102ff
Martin Guerra Local <vernonguerra27@gmail.com>
2025-12-07 12:53:07 -0300

    mejoras

--- src/main/java/com/guerram/MyFocusTime/config/JwtUtil.java
+++ src/main/java/com/guerram/MyFocusTime/config/JwtUtil.java
@@ -1,7 +1,6 @@
 package com.guerram.MyFocusTime.config;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
@@ -11,14 +10,23 @@ import java.util.Date;
 @Component
 public class JwtUtil {
 
-    private final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final Key SECRET_KEY;
+
+    public JwtUtil() {
+        // Leer la clave de entorno
+        String secret = System.getenv("JWT_SECRET");
+        if (secret == null || secret.isEmpty()) {
+            throw new RuntimeException("JWT_SECRET no está configurada en las variables de entorno");
+        }
+        SECRET_KEY = Keys.hmacShaKeyFor(secret.getBytes()); // Crear Key a partir de la variable
+    }
 
     public String generateToken(Long userId, String mail){
         return Jwts.builder()
                 .setSubject(mail)
                 .claim("userId", userId)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis()+1000 * 60 *60 *10))//10hs
+                .setExpiration(new Date(System.currentTimeMillis()+1000 * 60 *60 *10)) // 10hs
                 .signWith(SECRET_KEY)
                 .compact();
     }
@@ -31,6 +39,7 @@ public class JwtUtil {
                 .getBody()
                 .get("userId", Long.class);
     }
+
     public String extractMail(String token){
         return Jwts.parserBuilder()
                 .setSigningKey(SECRET_KEY)
